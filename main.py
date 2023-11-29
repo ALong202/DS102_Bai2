069d7a9ffff65697f4339e663be783ae7bf1fb1a
ALong202 <147915024+ALong202@users.noreply.github.com>
2023-11-29 10:38:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 class_list = {'0': 'Normal','1':'Pneumonia'}
+
+input = open('', 'rb')
+model = plk.load(input)
+
+
+st.header('Upload a image')
+image = st.file_uploader('', type= (['png','jpg','jpeg']))
+
+if image is not None:
+  image = Image.open(image)
+  st.image(image, caption = 'Test image')
+
