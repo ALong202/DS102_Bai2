fb2e5676ad753a931b8cad189a2487df64f3f108
ALong202 <147915024+ALong202@users.noreply.github.com>
2023-11-29 10:52:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ st.title('Pneumonia Detection')
 input = open('lrc_xray.pkl', 'rb')
 model = plk.load(input)
 
-
 st.header('Upload an image')
 image = st.file_uploader('choose an image', type= (['png','jpg','jpeg']))
 
@@ -21,4 +20,25 @@ if image is not None:
   image = Image.open(image)
   st.image(image, caption = 'Test image')
 
+if st.button('Predict'):
+  image = image.resize((227*227*3,1))
+  vector = np.array(image)
+  st.write(model.predict(vector))
+  
+  st.header('Result')
+  st.text(label)
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
